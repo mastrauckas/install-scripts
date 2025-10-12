0311a83e4ea048fa79725f918e8f5266caa4b5d1
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2025-10-12 13:41:05 -0400

    Replacing the saving of the file.

--- windows/install.ps1
+++ windows/install.ps1
@@ -183,15 +183,20 @@ function Update-Pwsh7Profile {
    # Define the import line to add
    $importLine = "Import-Module `"$configRepoPath\powershell\main_script.ps1`""
 
-   # Append the import line only if it doesn't already exist in the profile
-   # if (-not (Select-String -Path $pwsh7Profile -Pattern [regex]::Escape($importLine) -Quiet)) {
-   #    Write-Host "Adding import line to PowerShell 7 profile..."
-   #    Add-Content -Path $pwsh7Profile -Value $importLine
-   #    Write-Host "Import line added successfully."
-   # }
-   # else {
-   #    Write-Host "Import line already exists in PowerShell 7 profile. No changes made."
-   # }
+   if (-not ($existing -contains $importLine)) {
+      # Ensure new line if file already has content
+      if ($existing.Count -gt 0) {
+         Add-Content -Path $pwsh7Profile -Value "`n$importLine"
+      }
+      else {
+         Add-Content -Path $pwsh7Profile -Value $importLine
+      }
+      Write-Host "Adding import line to PowerShell 7 profile..."
+      Write-Host "Import line added successfully."
+   }
+   else {
+      Write-Host "Import line already exists in PowerShell 7 profile. No changes made."
+   }
 }
 
 
